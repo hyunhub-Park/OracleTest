1a00bac7b5ead52d8e6f8f6ade10dbac9005272f
hyunhub <nuyhos_ambi@naver.com>
2024-11-14 09:36:52 +0900

    [append]hr1113.sql

--- hr1113_2.sql
+++ hr1113_2.sql
@@ -40,6 +40,38 @@ INSERT INTO dept02 VALUES(dept_seq.NEXTVAL, '총무과', 2);
 INSERT INTO dept02 VALUES(dept_seq.NEXTVAL, '서무과', 3);
 INSERT INTO dept02 VALUES(dept_seq.NEXTVAL, '교육과', 4);  /* maxvalue가 30이기 때문에, 40이 될 값은 들어갈 수 없음. */
 
+-- sequence 수정하기. --
+ALTER SEQUENCE dept_seq
+MAXVALUE 100;
+
+SELECT * FROM user_sequences WHERE sequence_name='dept_seq';
+
+-- sequence 삭제하기. --
+DROP SEQUENCE dept_seq;
+
+-- data dictionary에서 index 확인하기. --
+SELECT * FROM user_indexes WHERE TABLE_NAME='EMPLOYEES';
+SELECT * FROM user_ind_columns WHERE TABLE_NAME='EMPLOYEES';
+
+SELECT * FROM EMPLOYEES WHERE employee_id=100;
+
+DROP TABLE emp10;
+
+CREATE TABLE emp10
+AS
+SELECT * FROM employees WHERE 1=1;
+
+SELECT * FROM EMP10 WHERE employee_id=100;
+
+-- index 생성하기. --
+SELECT * FROM user_ind_columns WHERE TABLE_NAME='EMP10';
+CREATE UNIQUE INDEX EMP10_EMPLOYEE_ID_IX
+ON EMP10(EMPLOYEE_ID);
+
+-- index 삭제하기. --
+DROP INDEX EMP10_EMPLOYEE_ID_IX;
+
+
 
 
 
