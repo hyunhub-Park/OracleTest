c0e8e90a9831ff13dbd6d7676f57d4e7f88c00d6
hyunhub <nuyhos_ambi@naver.com>
2024-11-24 04:37:46 +0900

    [create]CAFEDB 사용자 생성.

--- SYSTEM_사용자생성.sql
+++ SYSTEM_사용자생성.sql
@@ -5,3 +5,18 @@ CREATE USER SHOPPINGMALL IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 
     DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
     TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
 GRANT connect, resource, dba TO SHOPPINGMALL; -- 권한 부여
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER SUBJECTDB CASCADE; -- 기존 사용자 삭제
+CREATE USER SUBJECTDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
+    TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
+GRANT connect, resource, dba TO SUBJECTDB; -- 권한 부여
+
+--------------------- CAFE DB ---------------------------
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER CAFEDB CASCADE; -- 기존 사용자 삭제
+CREATE USER CAFEDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
+    TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
+GRANT connect, resource, dba TO CAFEDB; -- 권한 부여
