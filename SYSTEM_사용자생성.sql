96adf59372291bf5fc628b033598b88b1d566533
hyunhub <nuyhos_ambi@naver.com>
2024-12-04 11:40:45 +0900

    [create]SYSTEM_사용자생성.sql 웹유저 생성

--- SYSTEM_사용자생성.sql
+++ SYSTEM_사용자생성.sql
@@ -20,3 +20,20 @@ CREATE USER CAFEDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
     DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
     TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
 GRANT connect, resource, dba TO CAFEDB; -- 권한 부여
+
+--------------------- TEAMPROJECT_02 ---------------------------
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER TEAMPROJECT_02 CASCADE; -- 기존 사용자 삭제
+CREATE USER TEAMPROJECT_02 IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
+    TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
+GRANT connect, resource, dba TO TEAMPROJECT_02; -- 권한 부여
+
+
+--------------------- 웹 프로젝트 사용자계정 생성하기. ---------------------------
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER webuser CASCADE; -- 기존 사용자 삭제
+CREATE USER webuser IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS    -- 데이터 저장소. --
+    TEMPORARY TABLESPACE TEMP;  -- 임시 저장 장소. --
+GRANT connect, resource, dba TO webuser; -- 권한 부여
